c958696dc7f34ba5ebb2e46d5a253556e1c9e824
NgChiseng <ngchiseng@gmail.com>
2017-05-23 14:20:42 -0400

    MLT-01 #comment Create Main activity with TourActivity, and initialize TourSharePreference.

---
+++ app/src/main/java/com/idbc/ngchiseng/mainlogintest/MainActivity.java
@@ -0,0 +1,37 @@
+package com.idbc.ngchiseng.mainlogintest;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    /* --- Here, its will declare the private variables of the class
+     */
+    private Boolean visited;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /* --- This will declare TourSharedPreference and access to the preferences files named "Tour"
+        and the MODE_PRIVATE by 0
+         */
+        final SharedPreferences initTour = getSharedPreferences("Tour", 0);
+        /* --- This will get the TourSharedPreference to the preferences access files named visited,
+        and if no exist the preference "visited" , the return the default value false.
+         */
+        visited = initTour.getBoolean("visited", false);
+
+        if (visited){
+
+        } else {
+            Intent i = new Intent(MainActivity.this, TourActivity.class);
+            i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(i);
+            finish();
+        }
+    }
+}
